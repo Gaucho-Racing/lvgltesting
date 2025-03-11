214ba7aa55d35ff03200243bb9657e92618a9fab
Casey Zwicker <caseypzwicker@gmail.com>
2025-03-10 23:30:57 -0700

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <SDL2/SDL.h>
+
+#include "lvgl/lvgl.h"
+
+int main() {
+    lv_init();
+
+    lv_sdl_window_create(800, 480);
+
+    uint32_t idle_time;
+    while(1) {
+        idle_time = lv_timer_handler();
+        SDL_Delay(idle_time);
+    }
+}
