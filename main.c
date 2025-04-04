e7ac758b4c7741eb764723863c6f64203e84dadd
Jason Wang <jasonw123451@gmail.com>
2025-04-03 21:49:17 -0700

    adding lvgl styles to flexrow 1 and 2

--- main.c
+++ main.c
@@ -7,29 +7,42 @@ int main() {
 
     lv_sdl_window_create(800, 480);
 
+    // styles
+    static lv_style_t screenStyle;
+    lv_style_init(&screenStyle);
+
+    static lv_style_t flexRowStyle;
+    lv_style_init(&flexRowStyle);
+    lv_style_set_width(&flexRowStyle, 500);
+    lv_style_set_flex_flow(&flexRowStyle, LV_FLEX_FLOW_ROW);
+    lv_style_set_layout(&flexRowStyle, LV_LAYOUT_FLEX);
+    lv_style_set_flex_grow(&flexRowStyle, 1); // Specifically in context of columnn
+
     // setup code here
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x39FF14), LV_PART_MAIN);
     lv_obj_set_layout(lv_screen_active(), LV_LAYOUT_FLEX);
     lv_obj_set_flex_flow(lv_screen_active(), LV_FLEX_FLOW_COLUMN);
     lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0x000000), LV_PART_MAIN);
 
     lv_obj_t * flexRow1 = lv_obj_create(lv_screen_active());
-    lv_obj_set_layout(flexRow1, LV_LAYOUT_FLEX);
-    lv_obj_set_flex_flow(flexRow1, LV_FLEX_FLOW_ROW);
-    lv_obj_set_flex_grow(flexRow1, 1);
+    lv_obj_add_style(flexRow1, &flexRowStyle, 0);
+    lv_obj_set_flex_align(flexRow1, LV_FLEX_ALIGN_SPACE_AROUND, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_SPACE_AROUND);
 
     lv_obj_t * flexRow2 = lv_obj_create(lv_screen_active());
-    lv_obj_set_layout(flexRow2, LV_LAYOUT_FLEX);
-    lv_obj_set_flex_flow(flexRow2, LV_FLEX_FLOW_ROW);
-    lv_obj_set_flex_grow(flexRow2, 1);
+    lv_obj_add_style(flexRow2, &flexRowStyle, 0);
+    lv_obj_set_flex_align(flexRow2, LV_FLEX_ALIGN_SPACE_AROUND, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_SPACE_AROUND);
 
     lv_obj_t * label1 = lv_label_create(flexRow1);
     lv_label_set_text(label1, "Row 1 Test");
+    lv_obj_set_flex_grow(label1, 1);
     lv_obj_t * speed = lv_label_create(flexRow1);
     lv_label_set_text(speed, "Speed aaaaaaaaaaaaaaaa ");
+    lv_obj_set_flex_grow(speed, 1);
     
 
     lv_obj_t * label2 = lv_label_create(flexRow2);
     lv_label_set_text(label2, "Row 2 Test");
+    lv_obj_set_flex_grow(label2, 1);
 
     uint32_t idle_time;
     while(1) {
