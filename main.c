fd28034d976f17e1b9dd34fbdf292438c8453b80
Jason Wang <jasonw123451@gmail.com>
2025-04-03 19:50:02 -0700

    spacing

--- main.c
+++ main.c
@@ -10,10 +10,7 @@ int main() {
 
 
     // setup code here
-    lv_obj_t * label = lv_label_create(lv_screen_active());
-    lv_label_set_text(label, "Hello world");
-    lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
     uint32_t idle_time;
     while(1) {
         // looped code here
