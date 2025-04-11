e5c4f6f47018fdd6394cddcb30a30a6adcaeddcf
Bailey Say <baileyasay@gmail.com>
2025-04-10 20:57:10 -0700

    reset exmaples
    Co-authored-by: Jason Wang <JayWango@users.noreply.github.com>

--- main.c
+++ main.c
@@ -2,32 +2,6 @@
 #include <SDL2/SDL.h>
 #include "lvgl/lvgl.h"
 
-static void event_cb(lv_event_t * e)
-{
-    LV_LOG_USER("Clicked");
-
-    static uint32_t cnt = 1;
-    lv_obj_t * btn = lv_event_get_target(e);
-    lv_obj_t * label = lv_obj_get_child(btn, 0);
-    lv_label_set_text_fmt(label, "%"LV_PRIu32, cnt);
-    cnt++;
-}
-
-/**
- * Add click event to a button
- */
-void lv_example_event_1(void)
-{
-    lv_obj_t * btn = lv_button_create(lv_screen_active());
-    lv_obj_set_size(btn, 100, 50);
-    lv_obj_center(btn);
-    lv_obj_add_event_cb(btn, event_cb, LV_EVENT_CLICKED, NULL);
-
-    lv_obj_t * label = lv_label_create(btn);
-    lv_label_set_text(label, "Click me!");
-    lv_obj_center(label);
-}
-
 int main() {
 
     static char data[10] = "1000";
@@ -91,17 +65,9 @@ int main() {
     
     // --- LVGL Main Loop --- 
     uint32_t idle_time;
-    int i = 0;
 
     while(1) {
 
-        if (i == 1000) {
-            lv_memcpy(data, new_data, 10);
-            lv_label_set_text_static(speed, data);
-        } 
-        i++;
-        // looped code here
-        
         lv_task_handler();
         lv_delay_ms(5);
         // idle_time = lv_timer_handler();
